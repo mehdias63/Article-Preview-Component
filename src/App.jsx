f36ab26b6e0cd1cfd47afd34d65d05ab510df5b2
mehdias63 <137681653+mehdias63@users.noreply.github.com>
2024-02-17 12:36:38 +0330

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,12 @@ function App() {
     setIsSubmited(true);
   }
   return (
-    <main className="bg-light-blue min-h-screen flex justify-center items-center px-6 py-16 lg:px-[8rem]">
-      <div className="md:flex font-man bg-white rounded-xl w-[20.4375rem] md:w-[45.625rem] md:h-[17.5rem] ">
-        <div className="rounded-t-xl md:rounded-bl-xl">
-          <img
-            src="/images/drawers.jpg"
-            alt="drawers"
-            className="rounded-t-xl md:rounded-tr-none md:rounded-bl-xl md:w-[45rem] md:h-[17.5rem]"
-          />
-        </div>
+    <main className="bg-light-blue min-h-screen flex justify-center items-center px-6 py-16 md:p-16 lg:px-[8rem]">
+      <div className="md:flex font-man bg-white rounded-xl shadow-[0_40px_40px_-10px_rgba(201,213,225,0.50)] md:max-w-[46rem] ">
+        <div className="rounded-t-xl md:rounded-bl-xl  h-[14rem] w-full overflow-hidden md:h-auto bg-[url(/images/drawers.jpg)] bg-cover"></div>
         <div>
-          <div className="p-8">
-            <p className="text-base text-dark-gray font-bold leading-6 tracking-[0.0125rem] md:text-xl md:leading-7 md:tracking-[0.01563rem] mb-4 ">
+          <div className="p-8 flex flex-col gap-3">
+            <p className="text-base text-dark-gray font-bold leading-6 tracking-[0.0125rem] md:text-xl md:leading-7 md:tracking-[0.01563rem]">
               Shift the overall look and feel by adding these wonderful touches
               to furniture in your home
             </p>
@@ -33,7 +27,7 @@ function App() {
           </div>
           {isSubmited ? (
             <>
-              <div className="flex items-center gap-3 p-8 md:p-0 md:px-8">
+              <div className="flex items-center gap-4 px-8 py-5 md:py-8 ">
                 <img
                   src="/images/avatar-michelle.jpg"
                   className="w-10 h-10 rounded-full"
@@ -55,8 +49,27 @@ function App() {
             </>
           ) : (
             <>
+              <div className="hidden md:flex items-center gap-4 px-8 py-5 md:py-8 ">
+                <img
+                  src="/images/avatar-michelle.jpg"
+                  className="w-10 h-10 rounded-full"
+                  alt="avatar"
+                />
+                <div className="leading-5 tracking-[0.00763rem] text-[0.8125rem]">
+                  <h1 className=" text-dark-gray font-bold ">
+                    Michelle Appleton
+                  </h1>
+                  <p className="text-light-gray font-medium">28 Jun 2020</p>
+                </div>
+                <div
+                  onClick={handleSubmit}
+                  className="w-8 h-8 bg-light-blue rounded-full flex justify-center items-center ml-auto"
+                >
+                  <img src="/images/icon-share.svg" alt="icon" />
+                </div>
+              </div>
               <div className="relative">
-                <div className="md:absolute md:left-[18rem] md:bottom-7 bg-dark-gray flex gap-5 p-6 md:p-3 rounded-b-xl md:rounded-xl  md:h-[3.4375rem]">
+                <div className="bg-dark-gray flex gap-5 px-6 py-5 rounded-b-xl md:absolute md:-right-[3.05rem] md:bottom-[5.5rem] md:left-auto md:h-auto md:w-auto md:rounded-xl">
                   <p className="text-light-gray font-medium text-[0.8125rem] leading-5 tracking-[0.3125rem]">
                     SHARE
                   </p>
@@ -71,8 +84,8 @@ function App() {
                   >
                     <IconShare className="fill-white w-4" />
                   </div>
+                  <div className="border-t-dark-gray hidden md:block border-[1rem] border-transparent absolute left-[50%] top-[3.5rem]"></div>
                 </div>
-                <div className="border-t-dark-gray hidden md:block border-[1rem] border-transparent absolute left-[24rem] bottom-[1px]"></div>
               </div>
             </>
           )}
--- src/components/IconShare.jsx
+++ src/components/IconShare.jsx
@@ -3,7 +3,6 @@ import React from "react"
 export default function IconShare({ className }) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
       width="100%"
       height="100%"
       fill="currentColor"
